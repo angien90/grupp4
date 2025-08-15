fe14d0eeabeeff6e244c23ca0fbb95db92c9d0db
AgnesWilson <agnes.wilson97@hotmail.com>
2025-08-15 11:26:01 +0200

    ✨ Basic styling header
    Added basic stuyling for the header :)

--- grupp4/src/pages/Layout.tsx
+++ grupp4/src/pages/Layout.tsx
@@ -2,6 +2,8 @@ import { Outlet } from "react-router";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 
+import '../css/header.css';
+
 export const Layout = () => {
   return (
     <>
