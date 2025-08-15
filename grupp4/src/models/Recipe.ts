ee5d183d65d21bc8837f40456abf984e7c8ffbc3
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2025-08-15 10:50:15 +0200

    Add description, ingredients, howto in class Recipe

--- grupp4/src/models/Recipe.ts
+++ grupp4/src/models/Recipe.ts
@@ -2,10 +2,16 @@ export class Recipe {
   name: string;
   id: number;
   img: string;
+  description: string;
+  ingredients: string;
+  howto: string;
 
-  constructor(name: string, id: number, img: string) {
+  constructor(name: string, id: number, img: string, howto: string, description: string, ingredients: string) {
     this.id = id;
     this.name = name;
     this.img = img;
+    this.description = description;
+    this.ingredients = ingredients;
+    this.howto = howto;
   }
 }
